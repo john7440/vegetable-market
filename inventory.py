73e74fd738b1d5c019de9284fc08a543d4fa0366
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-22 06:31:13 +0200

    ajout d'une fonction pour calculer la valeur total de l'inventaire + sa docstring

--- inventory.py
+++ inventory.py
@@ -5,3 +5,10 @@ class InventoryManager:
     product: str
     stock: int
     unit_price: float
+
+    def total_value(self):
+        """
+        This function calculates the total value of the inventory.
+        :return: the value of the inventory.
+        """
+        return self.unit_price * self.stock
