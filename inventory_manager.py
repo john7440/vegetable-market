e66c6046aa6c413e521f55a01b0c32a4f5339c17
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-22 07:43:57 +0200

    ajout d'une fonction display_inventory pour faire un affichage complet et formatté d l'inventaire

--- inventory_manager.py
+++ inventory_manager.py
@@ -5,3 +5,15 @@ from typing import List
 @dataclass
 class InventoryManager:
     items : List[Inventory] = field(default_factory=list)
+
+    def display_inventory(self) -> None:
+        """Display the inventory product by product and
+        the total value of the inventory"""
+        print('=' * 60)
+        print(f"{'Product':<20} | {'Price':<20} | {'Quantity':<20}")
+        print('-' * 60)
+        for item in self.items:
+            print(item.display())
+        print('=' * 60)
+        total = sum(item.total_value() for item in self.items)
+        print(f'Total inventory value: {total:.2f} €')
