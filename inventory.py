c53ab2aea7a14a8bbb9fca9490b9f6bdefac0f43
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-22 06:34:56 +0200

    ajout de typage pour total_value + docstring de classe

--- inventory.py
+++ inventory.py
@@ -2,11 +2,16 @@ from dataclasses import dataclass
 
 @dataclass
 class InventoryManager:
+    """
+    This class is made to manage an inventory composed
+    of products (vegetables and fruits), we have a definite
+    stock and a price per unit or per kg.
+    """
     product: str
     stock: int
     unit_price: float
 
-    def total_value(self):
+    def total_value(self) -> float:
         """
         This function calculates the total value of the inventory.
         :return: the value of the inventory.
