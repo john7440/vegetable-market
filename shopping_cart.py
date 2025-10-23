50ed5402072839bcb919c4cd49fd4970032114da
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-10-23 13:39:54 +0200

    added display function

--- shopping_cart.py
+++ shopping_cart.py
@@ -30,3 +30,15 @@ class ShoppingCart:
     def pay(self):
         from history import History
         self.owner.history_list.append(History(self.articles_list, self.owner, datetime.datetime.now().date()))
+
+    def display(self) -> None:
+        """Display the inventory product by product and
+        the total value of the inventory"""
+        print('=' * 60)
+        print(f"{'Product':<20} | {'Quantity':<20} | {'Price':<20} ")
+        print('-' * 60)
+        for item in self.articles_list.items:
+            print(item.display())
+        print('=' * 60)
+        total = sum(item.total_value() for item in self.articles_list.items)
+        print(f'Total of shopping cart: {total:.2f} €')
