4645ea68a3c095ddd740f121397e666778e65fd7
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-10-23 09:30:20 +0200

    added function get_by_date() to get all history by date (using person.get_history_by_date)

--- history.py
+++ history.py
@@ -1,9 +1,23 @@
 from __future__ import annotations
-
+from typing import List
+from dataclasses import dataclass
 import datetime
 
+from person import Person
 
+@dataclass
 class History:
     articles_list : 'InventoryManager'
     owner : 'Person'
-    date : datetime.datetime
+    date : datetime.date
+
+
+    @staticmethod
+    def get_by_date(date : datetime.date):
+        list_of_article_by_date : List['InventoryManager'] = list()
+        users = Person.clients
+
+        for u in users:
+            list_of_article_by_date += u.get_history_by_date(date)
+
+        return list_of_article_by_date
