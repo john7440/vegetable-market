7f8c91e9c0590d0470384e75599fa9949b4aa414
Rafael DA SILVA <rafael1893porto@gmail.com>
2025-10-23 11:24:12 +0200

    modified some imports place to avoid bugs
    add '' to Client attribute type to avoid recursive import bugs
    
    move import History to pay() for the same reason

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 import datetime
 from dataclasses import dataclass, field
-from typing import Optional
-
 from inventory import Inventory
 from inventory_manager import InventoryManager
 
