2cc6d11175505201b4385ceb7158479d78bba97f
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-10-24 05:45:13 +0200

    ajout de docstring + typage

--- history.py
+++ history.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-
 from copy import deepcopy
 from typing import List
 from dataclasses import dataclass, field
@@ -8,18 +7,34 @@ import datetime
 
 @dataclass
 class History:
+    """
+    This class represents a snapshot of a client's shopping history at a
+    specific date.
+    """
     from inventory_manager import InventoryManager
     from client import Client
+
     owner: Client
     articles_list : 'InventoryManager' = field(default_factory=InventoryManager)
     date : datetime.date = field(default_factory=datetime.date.today)
 
-    def __post_init__(self):
-        self.articles_list = deepcopy(self.owner.shopping_cart.articles_list)
+
+    def __post_init__(self) -> None:
+        """
+        This method initializes the history by coping the current shopping cart
+        of the client.
+        """
+        self.articles_list = deepcopy(self.owner.shopping_cart.articles_list)  # type: ignore
 
 
     @staticmethod
-    def get_by_date(date : datetime.date):
+    def get_by_date(date : datetime.date) -> List[History]:
+        """
+        This method retrieves all history across all client that
+        matches the given date.
+        :param date: The date to retrieve history for.
+        :return: A list of History entries.
+        """
         from client import Client
         list_of_article_by_date : List['History'] = list()
         users = Client.clients
